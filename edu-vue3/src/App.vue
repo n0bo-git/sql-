26bb4722b727ad70e64157a4a74c7b23e8b846a7
n0bo-git <3297785058@qq.com>
2026-06-02 13:33:06 +0800

    前端网页

---
+++ edu-vue3/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <button class = "btn">
+    按钮
+  </button>
+ <!-- <div class="button-example">
+    <div class="button-row">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
+ </div> -->
+</template>
+
+<style>
+  .btn{
+    @apply
+   bg-indigo-500
+   text-white
+    px-4
+    py-4
+    rounded-full
+    transition
+    duration-500
+    hover:(bg-yellow-500 text-green-500)
+    focus:(ring-6 ring-yellow-600);
+  }
+</style>
