226b66c3d7fed53882b161f8dffd71a4a378dd1f
n0bo-git <3297785058@qq.com>
2026-06-03 11:05:44 +0800

    测试响应式布局

--- edu-vue3/src/pages/UserLogin.vue
+++ edu-vue3/src/pages/UserLogin.vue
@@ -1,18 +1,27 @@
 <template>
-  <el-row class="min-h-screen bg-purple-500">
-    <el-col :lg="16" class="flex items-center justify-center">
-      <div>
-        <div class="font-bold text-5xl text-light-50">欢迎光临</div>
-          <div class="text-gray-50 text-sm">这是我们的课设</div>
-        </div>
+  <el-row :gutter="10">
+    <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
+      <div class="grid-content bg-purple-500" />
     </el-col>
-    <el-col :lg="8" class="bg-yellow-200 flex items-center justify-center flex-col">
-      <h2 class="font-bold text-3xl text-gray-500">欢迎回家</h2>
-        <div class="flex items-center justify-center my-5 text-white space-x-2">
-          <span class="h-[1px] w-16 bg-gray-200"></span>
-          <span>账号密码登录</span>
-          <span class="h-[1px] w-16 bg-gray-200"></span>
-        </div>
+    <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+      <div class="grid-content bg-purple-500" />
+    </el-col>
+    <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+      <div class="grid-content bg-purple-500" />
+    </el-col>
+    <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
+      <div class="grid-content bg-purple-500" />
     </el-col>
   </el-row>
 </template>
+
+<style>
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
