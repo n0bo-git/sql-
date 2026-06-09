fac082e56bdb039a4a047398627c4129e761c67f
n0bo-git <3297785058@qq.com>
2026-06-09 16:05:05 +0800

    更新图标

--- edu-vue3/vite.config.js
+++ edu-vue3/vite.config.js
@@ -3,15 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+//import vueDevTools from 'vite-plugin-vue-devtools'
 import WindiCSS from 'vite-plugin-windicss'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue({
+      devtools: false,
+    }),
+    //vueDevTools(),
     WindiCSS(),
   ],
   resolve: {
